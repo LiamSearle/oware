a337c4726567909bac83d718862120efefb120a4
Luke Wilkinson <lukejamesw@gmail.com>
2019-03-14 22:52:32 +0200

    practicing for test added things to main

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -23,6 +23,7 @@ type player = {
 type game = {//has 12 houses & 2 players
   board:(house * house * house * house * house * house * house * house * house * house * house * house * house * house)
   players: (player * player)
+  //toWin: int <- 25
 }
  //--------------------------------------End Types--------------------------
  
@@ -57,13 +58,20 @@ let gameState board = failwith "Not implemented"
 
 [<EntryPoint>]
 let main _ =
-    let x = {player.score = 5; side = StartingPosition.North; isTurn = true; victory = false }
-    let y = {house.number = 12; numSeeds = 14}
-    let z = {house.number = 4; numSeeds = 52}
-    let z = {z with numSeeds = 14}
-    let {house.number = 3} = y
+    let player1 = {player.score = 5; side = StartingPosition.North; isTurn = true; victory = false }
+    let player2 = {player.score = 0; side = StartingPosition.South; isTurn = false; victory = false }
+
+    let house1 = {house.number = 1; numSeeds = 4}
+    
+    let house2 = {house.number = 2; numSeeds = 4}
+
+    let house3 = {house2 with number = 3}
+    let house3 = {house3 with numSeeds = house3.numSeeds+1}
+    
+    let {house.number = 3} = house3
+    
     
 
     printfn "Hello from F#!"
-    printfn "An int: %i." x.score
+    printfn "Player 1 score: %i. Player 2 score: %i." player1.score player2.score
     0 // return an integer exit code
