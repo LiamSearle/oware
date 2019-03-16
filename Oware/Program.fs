9e57b9df0ced42bc6e72778c87e3dbe5be566465
Liam Searle <liamsearle7@gmail.com>
2019-03-16 18:44:54 +0200

    changing things about types and game output

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -4,39 +4,56 @@
 module Oware
 
  //--------------------------------------Types--------------------------
-type house = {
-   number: int
-   numSeeds: int
-}
+//type House = {
+   //number: int
+  // numSeeds: int
+//}
 
 type StartingPosition =
     | North
     | South
 
-type player = {
+type Player = {
   score: int
   side: StartingPosition
   isTurn: bool
   victory: bool
 }
+type Board = {
+  House1: int; House2: int; House3: int; House4: int; House5: int; House6: int; 
+  House7: int; House8: int; House9: int; House10: int; House11: int; House12: int; 
+}
 
-type game = {//has 12 houses & 2 players
-  board:(house * house * house * house * house * house * house * house * house * house * house * house * house * house)
-  players: (player * player)
-  toWin: int //player wins with 25 seeds.       Should we keep this here?
+type Game = {//has 12 Houses & 2 Players
+  gameBoard: Board
+  Player1: Player
+  Player2: Player
+  toWin: int //Player wins with 25 seeds.       Should we keep this here?
 }
  //--------------------------------------End Types--------------------------
  
+let outputGame game = //function that takes in a game and prints out the Board and scores
+    printfn "|________Player 1 score_________|" 
+    printfn "|-----------|~~%i~~|------------|" game.Player1.score
+    printfn "|-------------------------------|"
+    printfn "|-[%i]-[%i]-[%i]-[%i]-[%i]-[%i]-|" game.gameBoard.House12 game.gameBoard.House11 game.gameBoard.House10 game.gameBoard.House9 game.gameBoard.House8 game.gameBoard.House7 
+    printfn "|-------------------------------|" //start bottom left to move in counter-clockwise direction
+    printfn "|-[%i]-[%i]-[%i]-[%i]-[%i]-[%i]-|" game.gameBoard.House1 game.gameBoard.House2 game.gameBoard.House3 game.gameBoard.House4 game.gameBoard.House5 game.gameBoard.House6  
+    printfn "|-------------------------------|"
+    printfn "|-----------|~~%i~~|------------|" game.Player2.score
+    printfn "|________Player 2 score_________|"
+    ()
 
-(*getSeeds, which accepts a house number and a board, and returns the number of
-seeds in the specified house*)
-//gets seeds at that house
-let getSeeds n board = failwith "Not implemented"
 
-(*useHouse, which accepts a house number and a board, and makes a move using
-that house.*)
-//getSeeds, count the seeds and itt. through them to distribute to houses greater than the orig, can't use foe's house
-let useHouse n board = 
+(*getSeeds, which accepts a House number and a Board, and returns the number of
+seeds in the specified House*)
+//gets seeds at that House
+let getSeeds n Board = failwith "Not implemented"
+
+(*useHouse, which accepts a House number and a Board, and makes a move using
+that House.*)
+//getSeeds, count the seeds and itt. through them to distribute to Houses greater than the orig, can't use foe's House
+let useHouse n Board = 
     let rec cnt j k = 
         match j/10 = 0 with
         |true -> k
@@ -48,38 +65,39 @@ let useHouse n board =
 person in the StartingPosition starts the game*)
 let start position  = failwith "Not implemented"
 
-(*score, which accepts a board and gives back a tuple of (southScore , northScore)*)
-let score board = failwith "Not implemented"
+(*score, which accepts a Board and gives back a tuple of (southScore , northScore)*)
+let score Board = failwith "Not implemented"
 
-(*gameState, which accepts a board and gives back a string that tells us about the
+(*gameState, which accepts a Board and gives back a string that tells us about the
 state of the game. Valid strings are “South’s turn”, “North’s turn”, “Game ended in a
 draw”, “South won”, and “North won”.*)
-let gameState board = failwith "Not implemented"
+let gameState Board = failwith "Not implemented"
 
 [<EntryPoint>]
 let main _ =
-    let player1 = {player.score = 5; side = StartingPosition.North; isTurn = true; victory = false }
-    let player2 = {player.score = 0; side = StartingPosition.South; isTurn = false; victory = false }
+    (*let Player1 = {Player.score = 5; side = StartingPosition.North; isTurn = true; victory = false }
+    let Player2 = {Player.score = 0; side = StartingPosition.South; isTurn = false; victory = false }
 
-    let house1 = {house.number = 1; numSeeds = 4}
-    let house2 = {house.number = 2; numSeeds = 4}
+    let House1 = {House.number = 1; numSeeds = 4}
+    let House2 = {House.number = 2; numSeeds = 4}
 
-    let house3 = {house2 with number = 3}
-    let house3 = {house3 with numSeeds = house3.numSeeds+1}
+    let House3 = {House2 with number = 3}
+    let House3 = {House3 with numSeeds = House3.numSeeds+1}
     
-    let {house.number = 3} = house3
+    let {House.number = 3} = House3*)
     
 
     //------------------------------------Game output-------------------------------------------
+    (*
     printfn "|________Player 1 score_________|" 
-    printfn "|-----------|~~%i~~|------------|" player1.score
+    printfn "|-----------|~~%i~~|------------|" Player1.score
     printfn "|-------------------------------|"
-    printfn "|-[%i]-[%i]-[%i]-[%i]-[%i]-[%i]-|" house12.number house11.number house10.number house9.number house8.number house7.number 
+    printfn "|-[%i]-[%i]-[%i]-[%i]-[%i]-[%i]-|" House12.number House11.number House10.number House9.number House8.number House7.number 
     printfn "|-------------------------------|" //start bottom left to move in counter-clockwise direction
-    printfn "|-[%i]-[%i]-[%i]-[%i]-[%i]-[%i]-|" house1.number house2.number house3.number house4.number house5.number house6.number 
+    printfn "|-[%i]-[%i]-[%i]-[%i]-[%i]-[%i]-|" House1.number House2.number House3.number House4.number House5.number House6.number 
     printfn "|-------------------------------|"
-    printfn "|-----------|~~%i~~|------------|" player2.score
-    printfn "|________Player 2 score_________|" 
+    printfn "|-----------|~~%i~~|------------|" Player2.score
+    printfn "|________Player 2 score_________|" *)
     //----------------------------------Game output end----------------------------------------
 
     0 // return an integer exit code
